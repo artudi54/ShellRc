659cf098b67119fcb793cce4133e67e32ca827f9
Artur Pietrzyk <artudi54@gmail.com>
2019-02-08 19:03:01 +0100

    update

--- shell/prompt/themes/informational.theme.sh
+++
@@ -1,9 +0,0 @@
-unset PS1
-unset PROMPT
-unset PROMPT_COMMAND
-
-if [ -n "$BASH_VERSION" ]; then
-    PS1='[$?]$ '
-elif [ -n "$ZSH_VERSION" ]; then
-    PS1=$'%{$fg[cyan]%}%D{%c}%{$reset_color%}\n%{$fg[green]%}%/%{$reset_color%}\n%{$fg_bold[yellow]%}[$?]%{$reset_color%}$ '
-fi
