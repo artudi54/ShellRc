508e92c28ed92c4174300e27ee227d3a79b08439
Artur Pietrzyk <artudi54@gmail.com>
2019-03-26 20:51:12 +0100

    update

--- configure/install/centos/init.sh
+++ configure/install/centos/init.sh
@@ -2,5 +2,5 @@
 sudo yum localinstall -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm --eval %{centos_ver}).noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm --eval %{centos_ver}).noarch.rpm
 sudo yum install -y https://centos$(rpm --eval %{centos_ver}).iuscommunity.org/ius-release.rpm
 sudo rpm -Uvh http://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el$(rpm --eval %{centos_ver}).noarch.rpm
-sudo yum --enablerepo=gf-plus makecache
+sudo yum --enablerepo=gf-plus update
 
