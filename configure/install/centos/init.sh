9463488371db6b8fac143328752e063930342e89
Artur Pietrzyk <artudi54@gmail.com>
2019-03-26 18:17:01 +0100

    update

--- configure/install/centos/init.sh
+++ configure/install/centos/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bsh
 sudo yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm --eval %{centos_ver}).noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm --eval %{centos_ver}).noarch.rpm
 sudo yum install -y https://centos$(rpm --eval %{centos_ver}).iuscommunity.org/ius-release.rpm
+sudo yum makecache
 
