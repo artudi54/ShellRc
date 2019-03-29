3f1dd9bcd243142bdcc888c116414f4b07b19a44
Artur Pietrzyk <artudi54@gmail.com>
2019-03-29 22:37:57 +0100

    update

---
+++ shell/plugins/entries/paki.plugin.sh
@@ -0,0 +1,2 @@
+source "$SHELLRC_DIR/shell/plugins/files/paki/paki.plugin.sh"
+
---
+++ shell/plugins/files/paki/paki.plugin.sh
@@ -0,0 +1,8 @@
+# paki plugin file
+
+# PATH addition
+export PATH="$PATH:$SHELLRC_DIR/shell/plugins/files/paki/paki/bin"
+
+# autocompletion
+source "$SHELLRC_DIR/shell/plugins/files/paki/paki/lib/completion/paki_completion.sh"
+
