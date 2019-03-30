fe6f73da57398d9c5087a85d2d4348f86208048c
Artur Pietrzyk <artudi54@gmail.com>
2019-03-30 15:25:13 +0100

    update

---
+++ shell/plugins/files/hooks/hooks.plugin.sh
@@ -0,0 +1,12 @@
+# Hooks support for bash.
+# Includes chpwd, precmd and preexec
+# Uses PROMPT_COMMAND - should not be used anywehere else
+
+# Currently supported: chpwd, precmd, preexec
+
+if [ ! -z "$BASH_VERSION" ]; then
+    # chpwd hook
+    source "$SHELLRC_DIR/shell/plugins/files/hooks/bash-chpwd.sh"
+    # modified version of https://github.com/rcaloras/bash-preexec
+    source "$SHELLRC_DIR/shell/plugins/files/hooks/bash-precmd-preexec.sh"
+fi
