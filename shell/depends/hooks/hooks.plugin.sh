175a3fc68b3e7570900d09f0d98bde4649fec3af
Artur Pietrzyk <artudi54@gmail.com>
2019-06-03 16:10:59 +0200

    update

--- shell/depends/hooks/hooks.plugin.sh
+++ shell/depends/hooks/hooks.plugin.sh
@@ -6,7 +6,7 @@
 
 if [ ! -z "$BASH_VERSION" ]; then
     # chpwd hook
-    source "$SHELLRC_DIR/shell/plugins/files/hooks/bash-chpwd.sh"
+    source "$SHELLRC_DIR/shell/depends/hooks/bash-chpwd.sh"
     # modified version of https://github.com/rcaloras/bash-preexec
-    source "$SHELLRC_DIR/shell/plugins/files/hooks/bash-precmd-preexec.sh"
+    source "$SHELLRC_DIR/shell/depends/hooks/bash-precmd-preexec.sh"
 fi
