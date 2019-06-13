efef894d6a5cea8cc0a259ef76bc55be5323e4af
Artur Pietrzyk <artudi54@gmail.com>
2019-06-13 23:19:03 +0200

    update

--- configure/preinstall/debian.sh
+++ configure/preinstall/debian.sh
@@ -2,8 +2,10 @@
 
 # gcc-8 repository
 if ! apt-cache show '^gcc-8$' 2>/dev/null 1>&2; then
-    file="/etc/apt/sources.list.d/jonathonf-ubuntu-gcc-8_0-xenial.list"
-    echo "Adding gcc-8 ppa"
-    printf "deb http://ppa.launchpad.net/jonathonf/gcc-8.0/ubuntu xenial main\n# deb-src http://ppa.launchpad.net/jonathonf/gcc-8.0/ubuntu xenial main\n" | sudo tee "$file"
+    gcc="/etc/apt/sources.list.d/jonathonf-ubuntu-gcc-8_0-xenial.list"
+    binutils="/etc/apt/sources.list.d/jonathonf-ubuntu-binutils-xenial.list"
+    echo "Adding gcc-8 and binutils ppa"
+    printf "deb http://ppa.launchpad.net/jonathonf/gcc-8.0/ubuntu xenial main\n# deb-src http://ppa.launchpad.net/jonathonf/gcc-8.0/ubuntu xenial main\n" | sudo tee "$gcc"
+    printf "deb http://ppa.launchpad.net/jonathonf/binutils/ubuntu xenial main\n# deb-src http://ppa.launchpad.net/jonathonf/binutils/ubuntu xenial main\n" | sudo tee "$binutils"
     sudo apt-get update
 fi
