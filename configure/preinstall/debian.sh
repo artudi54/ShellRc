4ed2d36587663b35d0e15ca174c3aa91caafefe2
Artur Pietrzyk <artudi54@gmail.com>
2019-06-13 23:25:29 +0200

    update

--- configure/preinstall/debian.sh
+++ configure/preinstall/debian.sh
@@ -7,5 +7,6 @@ if ! apt-cache show '^gcc-8$' 2>/dev/null 1>&2; then
     echo "Adding gcc-8 and binutils ppa"
     printf "deb http://ppa.launchpad.net/jonathonf/gcc-8.0/ubuntu xenial main\n# deb-src http://ppa.launchpad.net/jonathonf/gcc-8.0/ubuntu xenial main\n" | sudo tee "$gcc"
     printf "deb http://ppa.launchpad.net/jonathonf/binutils/ubuntu xenial main\n# deb-src http://ppa.launchpad.net/jonathonf/binutils/ubuntu xenial main\n" | sudo tee "$binutils"
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 4AB0F789CBA31744CC7DA76A8CF63AD3F06FC659
     sudo apt-get update
 fi
