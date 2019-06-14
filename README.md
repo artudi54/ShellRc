3fa415c08c6ccffe5c45bd7dd19e37887824c30f
Artur Pietrzyk <artudi54@gmail.com>
2019-06-14 18:36:10 +0200

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+## Supported Distributions
+- Ubuntu 16.04+
+- CentOS 6+
+- Debian 8+
+- Fedora 26+
+- Arch Linux and derivatives
+ShellRc may work on other distributions but it isn't guaranteed.
--- configure/postinstall/centos.sh
+++ configure/postinstall/centos.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 set -e
+
+if [ ! -f /usr/bin/python3 ]; then
+    if [ -f /usr/bin/python3.7 ]; then
+        VER="3.7"
+    elif [ -f /usr/bin/python3.6 ]; then
+        VER="3.6"
+    fi
+    if [ -z "$VER" ]; then
+        echo "Could not find python executable" 1>&2
+    fi
+    ln -s /usr/bin/python3.7 /usr/bin/python3
+fi
+
