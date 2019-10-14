002c069103096f189d2aa15cd6f47a00f6bf01a0
Artur Pietrzyk <artur.pietrzyk@aptiv.com>
2019-10-14 11:54:44 +0200

    update

--- shell/plugins/completions/completions.plugin.sh
+++ shell/plugins/completions/completions.plugin.sh
@@ -3,7 +3,9 @@
 if [ -n "$ZSH_VERSION" ]; then
     source "$SHELLRC_DIR/shell/plugins/completions/zsh-completions/zsh-completions.plugin.zsh"
     source "$SHELLRC_DIR/shell/plugins/completions/git-flow-completion/git-flow-completion.plugin.zsh"
+    source "$SHELLRC_DIR/shell/plugins/completions/git-extras-completion/git-extras-completion.zsh"
 elif [ -n "$BASH_VERSION" ]; then
     source "$SHELLRC_DIR/shell/plugins/completions/bash-completion/bash_completion"
     source "$SHELLRC_DIR/shell/plugins/completions/git-flow-completion/git-flow-completion.bash"
+    source "$SHELLRC_DIR/shell/plugins/completions/git-extras-completion/bash_completion.sh"
 fi
