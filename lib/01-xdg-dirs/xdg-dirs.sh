93f7568ad9edf63616aca25d84563b7f84294e72
Artur Pietrzyk <artur.pietrzyk@3deling.com>
2020-01-27 10:24:22 +0100

    update

--- lib/01-xdg-dirs/xdg-dirs.sh
+++ lib/01-xdg-dirs/xdg-dirs.sh
@@ -11,21 +11,21 @@ fi
 if [[ ! -v XDG_CONFIG_HOME ]]; then
     export XDG_CONFIG_HOME="$HOME/.config"
     if [[ ! -d "$XDG_CONFIG_HOME" ]]; then
-        mkdir -p "XDG_CONFIG_HOME"
+        mkdir -p "$XDG_CONFIG_HOME"
     fi
 fi
 
 if [[ ! -v XDG_CACHE_HOME ]]; then
     export XDG_CACHE_HOME="$HOME/.cache"
     if [[ ! -d "$XDG_CACHE_HOME" ]]; then
-        mkdir -p "XDG_CACHE_HOME"
+        mkdir -p "$XDG_CACHE_HOME"
     fi
 fi
 
 if [[ ! -v XDG_DATA_HOME ]]; then
     export XDG_DATA_HOME="$HOME/.local/share"
     if [[ ! -d "$XDG_DATA_HOME" ]]; then
-        mkdir -p "XDG_DATA_HOME"
+        mkdir -p "$XDG_DATA_HOME"
     fi
 fi
 
