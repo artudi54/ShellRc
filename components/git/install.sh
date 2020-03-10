21a6fd123a3c81126a99a0c75ee6911f2631e4ab
Artur Pietrzyk <artudi54@gmail.com>
2020-03-10 19:08:17 +0100

    update

--- components/git/install.sh
+++ components/git/install.sh
@@ -1,3 +1,9 @@
 dir="$(script-directory)"
 mkdir -p "$XDG_CONFIG_HOME/git"
-echo "[include]\n    path = \"$dir/gitconfig.ini\"" >"$XDG_CONFIG_HOME/git/config" 
+printf "[include]\tpath = \"$dir/gitconfig.ini\"\n" >"$XDG_CONFIG_HOME/git/config" 
+IFS=$'\n' read -p "Enter your name: " name
+read -p "Enter your email: " email
+git config --global include.path "$dir/gitconfig.ini"
+git config --global user.name "$name"
+git config --global user.email "$email"
+
---
+++ configure/newconf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ../lib/lib.sh
+./backup.sh ../components
+source ./install.sh ../components
