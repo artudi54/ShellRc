38eb0c31088e68c600f69bb527276da461d8afff
Artur Pietrzyk <artudi54@gmail.com>
2020-03-10 20:53:29 +0100

    update

--- components/konsole/install.sh
+++ components/konsole/install.sh
@@ -2,4 +2,5 @@
 dir="$(script-directory)"
 ln -s "$dir/konsole.ini" "$XDG_CONFIG_HOME/konsolerc"
 ln -s "$dir/config" "$XDG_DATA_HOME/konsole"
+git update-index --assume-unchanged "$dir/konsole.ini"
 
