abbda3342a9d1eedb2bd164c2f2f104cedf08988
Artur Pietrzyk <artudi54@gmail.com>
2020-04-01 15:16:31 +0200

    update

---
+++ components/ls/ls.sh
@@ -0,0 +1,11 @@
+# colours
+eval "$(dircolors "$(script-directory)/directory-colors.conf")"
+
+# ls aliases
+alias ls='ls --color=auto -vh'
+
+alias l='ls'
+alias la='ls -a'
+
+alias ll='ls -l'
+alias lla='ls -al'
--- components/shell/shell/aliases/ls.sh
+++
@@ -1,9 +0,0 @@
-# ls aliases
-alias ls='ls --color=auto -vh'
-
-alias l='ls'
-alias la='ls -a'
-
-alias ll='ls -l'
-alias lla='ls -alias'
-
