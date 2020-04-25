f9f54227f8f5c238d73bbb07ff600d7a5749247e
Artur Pietrzyk <artudi54@gmail.com>
2020-04-25 03:07:59 +0200

    update

--- components/shell/shell/plugins/syntax-highlighting/syntax-highlighting.plugin.sh
+++ components/shell/shell/plugins/syntax-highlighting/syntax-highlighting.plugin.sh
@@ -1,8 +1,5 @@
 # plugin enabling syntax highlighting in shell
 
-if [ -n "$BASH_VERSION" ]; then
-    #include "ble-0.3.0/ble.sh" --rcfile "$(script-directory)/blerc.sh"
-    :
-elif [ -n "$ZSH_VERSION" ]; then
+if [ -n "$ZSH_VERSION" ]; then
 	include "zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"
 fi
