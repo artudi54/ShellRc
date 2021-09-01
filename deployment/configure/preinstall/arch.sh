7786716af40d77afc52f8df74c63329b23289ca3
Artur Pietrzyk <artudi54@gmail.com>
2021-09-01 22:37:44 +0200

    update

--- deployment/configure/preinstall/arch.sh
+++ deployment/configure/preinstall/arch.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -e
+sudo pacman-key --init
 
 # Chaotic AUR
-if ! grep "http://chaotic.bangl.de" /etc/pacman.conf >/dev/null; then
-    sudo pacman-key --keyserver keys.mozilla.org -r 3056513887B78AEB
+if ! grep "chaotic-mirrorlist" /etc/pacman.conf >/dev/null; then
+    sudo pacman-key --keyserver keyserver.ubuntu.com -r 3056513887B78AEB
     sudo pacman-key --lsign-key 3056513887B78AEB
+    sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-'{keyring,mirrorlist}'.pkg.tar.zst'
     echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf >/dev/null
-    echo "Server = http://chaotic.bangl.de/\$repo/x86_64" | sudo tee -a /etc/pacman.conf >/dev/null
-    echo "Server = https://repo.kitsuna.net/x86_64" | sudo tee -a /etc/pacman.conf >/dev/null
+    echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf >/dev/null
 fi
 
 # Andontie AUR
 if ! grep "https://aur.andontie.net" /etc/pacman.conf >/dev/null; then
-    sudo pacman-key --recv-key EA50C866329648EE
-    sudo pacman-key --lsign-key EA50C866329648EE
+    sudo pacman-key --recv-key B545E9B7CD906FE3
+    sudo pacman-key --lsign-key B545E9B7CD906FE3
     echo "[andontie-aur]" | sudo tee -a /etc/pacman.conf >/dev/null
     echo "Server = https://aur.andontie.net/\$arch" | sudo tee -a /etc/pacman.conf >/dev/null
 fi
@@ -31,7 +32,7 @@ fi
 if ! grep "https://repo.archlinuxcn.org" /etc/pacman.conf >/dev/null; then
     echo "[archlinuxcn]" | sudo tee -a /etc/pacman.conf >/dev/null
     echo "Server = https://repo.archlinuxcn.org/\$arch" | sudo tee -a /etc/pacman.conf >/dev/null
-    sudo pacman -Syy && sudo pacman -S archlinuxcn-keyring
+    sudo pacman -Syy --noconfirm && sudo pacman -S --noconfirm archlinuxcn-keyring
 fi
 
 
@@ -42,7 +43,17 @@ if ! grep "https://userrepository.eu" /etc/pacman.conf >/dev/null; then
     echo "SigLevel = Optional TrustAll" | sudo tee -a /etc/pacman.conf >/dev/null
 fi
 
-pacman -Sy
+# ArchStrike
+if ! grep "/etc/pacman.d/archstrike-mirrorlist" /etc/pacman.conf >/dev/null; then
+    sudo pacman-key -r 9D5F1C051D146843CDA4858BDE64825E7CBC0D51
+    sudo pacman-key --lsign-key 9D5F1C051D146843CDA4858BDE64825E7CBC0D51
+    echo "[archstrike]" | sudo tee -a /etc/pacman.conf >/dev/null
+    echo "Server = https://mirror.archstrike.org/\$arch/\$repo" | sudo tee -a /etc/pacman.conf >/dev/null
+    sudo pacman -Syy --noconfirm && sudo pacman -S --noconfirm archstrike-keyring archstrike-mirrorlist
+    echo "Include = /etc/pacman.d/archstrike-mirrorlist" | sudo tee -a /etc/pacman.conf >/dev/null
+fi
+
+pacman -Syy --noconfirm
 
 # Install yay if not present
 if ! which yay 2>/dev/null 1>&2; then
