60cb23751da918e6453d7088f2026c938e99e1c0
Artur Pietrzyk <artudi54@gmail.com>
2022-03-10 01:59:01 +0100

    update

--- src/environment/path.sh
+++ src/environment/path.sh
@@ -1,12 +1,18 @@
-# Add .local/bin to path
-if [[ "$PATH" != *"$HOME/.local/bin"* ]]; then
-    export PATH="$PATH:$HOME/.local/bin"
-fi
+# Ensure .local/bin exists
+mkdir -p "$HOME/.local/bin"
 
-# Add .local/bin subdirectories to path
-for dir in "$HOME/.local/bin/"*; do
-    if [[ -d "$dir" ]] && [[ "$PATH" != *"$dir"* ]]; then
-        export PATH="$PATH:$dir"
+# Add .local/bin and its subdirectories to path
+binFiles=("$HOME/.local/bin" "$HOME/.local/bin/"*)
+addedPaths=""
+
+for ((idx=${#binFiles[@]}; idx>=0 ; idx-- )); do
+    if [[ -d "${binFiles[idx]}" ]] && [[ "$PATH" != *"${binFiles[idx]}"* ]]; then
+        addedPaths="${binFiles[idx]}:$addedPaths"
     fi
 done
+export PATH="${addedPaths}${PATH}"
+
+unset idx
+unset binFiles
+unset addedPaths
 
