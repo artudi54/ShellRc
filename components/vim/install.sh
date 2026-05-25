47cdce98bea914b37b07c4e83984acbe8dfbe8b0
artudi54 <artudi54@gmail.com>
2026-05-26 00:01:40 +0200

    Updated YouCompleteMe

--- components/vim/install.sh
+++ components/vim/install.sh
@@ -3,9 +3,3 @@ dir="$(script_directory)"
 #ycm
 python3 "$dir/plugins/YouCompleteMe/install.py" --all
 
-# color_coded
-rm -f "$dir/plugins/color_coded/CMakeCache.txt"
-rm -rf "$dir/plugins/color_coded/build"
-mkdir "$dir/plugins/color_coded/build"
-(cd "$dir/plugins/color_coded/build" && cmake .. && make install && make clean && make clean_clang)
-
