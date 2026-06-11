3b5b2538203993c056b76db26a4e9cb689b32a13
artudi54 <artudi54@gmail.com>
2026-06-11 17:38:56 +0200

    Added more stuff to tmux/screen status bar

--- components/screen/screenrc-v4.sh
+++ components/screen/screenrc-v4.sh
@@ -1,4 +1,4 @@
 source $SHELLRC_DIR/components/screen/screenrc.sh
 
 caption always "%{= bw}%-w%{= BW} %n %t %{-}%+w %-="
-hardstatus alwayslastline "%{= yk} [%S | $SHELL] %{= gW} $USER@%H  %-= %{= yk} %LD %d %LM %Y - %c "
+hardstatus alwayslastline "%{= yk} [%S | $SHELL] %{+b = gW} $USER@%H | %1` | %2` | up %3`  %-= %{= yk} %LD %d %LM %Y - %c "
--- components/screen/screenrc.sh
+++ components/screen/screenrc.sh
@@ -2,7 +2,12 @@ startup_message off
 defscrollback 20000
 
 caption always "%{7;4}%-w%{7;12} %n %t %{-}%+w %-="
-hardstatus alwayslastline "%{0;3} [%S | $SHELL] %{7;2} $USER@%H  %-= %{= yk} %LD %d %LM %Y - %c "
+hardstatus alwayslastline "%{0;3} [%S | $SHELL] %{+b 7;2} $USER@%H | %1` | %2` | up %3`  %-= %{= yk} %LD %d %LM %Y - %c "
+
+# Status bar info commands
+backtick 1 30 30 sh -c 'ip -4 route get 1.1.1.1 2>/dev/null | grep -oP "src \K[^ ]+" || hostname -I 2>/dev/null | cut -d" " -f1'
+backtick 2 10 10 sh -c 'free -h | awk "/Mem:/{print \$3\"/\"\$2}"'
+backtick 3 30 30 sh -c 'uptime -p | sed "s/^up //"'
 
 # Remove annoying binds
 bindkey -k k4
