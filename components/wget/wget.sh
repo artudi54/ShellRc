002ff926e9ef47033c8d29da79900d64cac32a3e
artudi54 <artudi54@gmail.com>
2026-06-15 14:45:25 +0000

    Simpler wgetrc generation

--- components/wget/wget.sh
+++ components/wget/wget.sh
@@ -3,8 +3,9 @@ __wget_dir="$(script_directory)"
 export WGETRC="$__wget_dir/wget.conf.gen"
 
 __wget-generate-wgetrc() {
-    cat "$__wget_dir/wget.conf" | envsubst > "$__wget_dir/wget.conf.gen"
+    envsubst > "$__wget_dir/wget.conf.gen" < "$__wget_dir/wget.conf"
 }
 
 __wget-generate-wgetrc
 precmd_functions+=(__wget-generate-wgetrc)
+
