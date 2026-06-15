4e2776bebe72dc4025a4ed326e50555a1b4b4a1a
artudi54 <artudi54@gmail.com>
2026-06-15 14:07:28 +0000

    Installing add-apt-repository for debian too

--- install/prepare/debian.sh
+++ install/prepare/debian.sh
@@ -4,12 +4,13 @@ set -e
 . /etc/os-release
 sudo apt-get update
 
+echo "Installing software-properties-common..."
+sudo apt-get install -y software-properties-common
+
 if [[ "$ID" == "debian" ]]; then
     echo "Enabling non-free repository..."
     sudo apt-add-repository -y non-free
 elif [[ "$ID" == "ubuntu" ]]; then
-    echo "Installing software-properties-common..."
-    sudo apt-get install -y software-properties-common
     echo "Adding PPA repositories..."
     sudo add-apt-repository -y ppa:jonathonf/vim
     sudo add-apt-repository -y ppa:longsleep/golang-backports
