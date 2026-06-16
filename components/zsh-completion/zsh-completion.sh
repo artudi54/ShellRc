9acb68a328f82347af9e6a8a44c9948ee372705e
artudi54 <artudi54@gmail.com>
2026-06-16 16:30:56 +0000

    Sped up zsh completion loading

--- components/zsh-completion/zsh-completion.sh
+++ components/zsh-completion/zsh-completion.sh
@@ -6,7 +6,17 @@ export skip_global_compinit=1
 # interactive only
 [[ $- != *i* ]] && return
 
-autoload -Uz compinit && compinit -d "$SHELLRC_STATE_DIR/zcompdump.log"
-autoload -Uz compaudit && compaudit
+# Guard against being loaded more than once
+[[ -v __SHELLRC_COMPINIT_LOADED ]] && return
+__SHELLRC_COMPINIT_LOADED=1
+
+autoload -Uz compinit && compinit -C -d "$SHELLRC_STATE_DIR/zcompdump.log"
 autoload -Uz bashcompinit && bashcompinit
+
+# Clean the guard flag after startup completes
+__shellrc_compinit_cleanup() {
+    unset __SHELLRC_COMPINIT_LOADED
+    precmd_functions=(${precmd_functions:#__shellrc_compinit_cleanup})
+}
+precmd_functions+=(__shellrc_compinit_cleanup)
 
