38b056742d8f086c1d86fce2a9d64a633e6f8a99
artudi54 <artudi54@gmail.com>
2026-06-16 15:25:34 +0000

    Fixed broken job count in bash

--- components/wget/wget.sh
+++ components/wget/wget.sh
@@ -3,7 +3,11 @@ __wget_dir="$(script_directory)"
 export WGETRC="$__wget_dir/wget.conf.gen"
 
 __wget-generate-wgetrc() {
-    envsubst > "$__wget_dir/wget.conf.gen" < "$__wget_dir/wget.conf"
+    # use shell parameter expansion instead of envsubst to avoid spawning
+    # an external process that inflates the job count in shell-prompt
+    local content pattern='$SHELLRC_STATE_DIR'
+    content=$(<"$__wget_dir/wget.conf")
+    printf '%s\n' "${content//$pattern/$SHELLRC_STATE_DIR}" > "$__wget_dir/wget.conf.gen"
 }
 
 __wget-generate-wgetrc
