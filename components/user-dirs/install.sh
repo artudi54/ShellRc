724ad5aa917cb849b427e13886c00ce4b65fcaa8
artudi54 <artudi54@gmail.com>
2026-06-23 21:39:17 +0200

    Added user-dirs.conf

--- components/user-dirs/install.sh
+++ components/user-dirs/install.sh
@@ -1,3 +1,4 @@
 ln -sfT "$(script_directory)/user-dirs.dirs" "$XDG_CONFIG_HOME/user-dirs.dirs"
 ln -sfT "$(script_directory)/user-dirs.locale" "$XDG_CONFIG_HOME/user-dirs.locale"
+ln -sfT "$(script_directory)/user-dirs.conf" "$XDG_CONFIG_HOME/user-dirs.conf"
 
